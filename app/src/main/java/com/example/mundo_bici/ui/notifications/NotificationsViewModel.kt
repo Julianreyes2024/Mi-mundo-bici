35590152660b19853726bf3d81e119aa264e530f
GoldStartHigher <stuartlee.dev@gmail.com>
2024-10-10 21:35:40 +0900

    start work Mimudo bici project

---
+++ app/src/main/java/com/example/mundo_bici/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.mundo_bici.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
